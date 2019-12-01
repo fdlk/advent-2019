f98b66d692efeb96eb05adfc58793dbee2c1d0be
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-01 10:22:50 +0100

    chore: run lein init app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to advent-2019
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/advent_2019/core.clj
@@ -0,0 +1,7 @@
+(ns advent-2019.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
