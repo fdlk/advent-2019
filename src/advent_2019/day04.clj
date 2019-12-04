e8c42a18065fa36a3308def294f1ae7cc6713009
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-04 16:09:34 +0100

    refactor(day04): use sets instead of partitions

--- src/advent_2019/day04.clj
+++ src/advent_2019/day04.clj
@@ -1,24 +1,14 @@
-(ns advent-2019.day04)
+(ns advent-2019.day04   (:require [clojure.string :refer [join]]))
 
-(def input (map str (range 402328 (inc 864247))))
+(def input (range 402328 (inc 864247)))
+(def digit-counts
+  (->> input
+       (map str)
+       (filter (fn is-monotonous [password] (= password (join (sort password)))))
+       (map frequencies)
+       (map vals)
+       (map set)))
+(def part1 (count (filter (fn [counts] (not= counts #{1})) digit-counts)))
+(def part2 (count (filter (fn [counts] (contains? counts 2)) digit-counts)))
 
-(defn is-same [[d1 d2]] (= d1 d2))
-(def is-not-same (complement is-same))
-(defn is-ascending [[d1 d2]] (<= (int d1) (int d2)))
-(defn is-same-middle [[d1 d2 d3 d4]] (and (is-same [d2 d3]) (is-not-same [d1 d2]) (is-not-same [d3 d4])))
-
-(defn is-valid-part1 [password]
-  (let [pairs (partition 2 1 password)]
-    (and
-     (some? (some is-same pairs))
-     (every? is-ascending pairs))))
-
-(defn is-also-valid-part2 [password]
-  (let [quads (partition 4 1 (concat "x" password "x"))]
-    (some? (some is-same-middle quads))))
-
-(def part1 (filter is-valid-part1 input))
-(def part2 (filter is-also-valid-part2 part1))
-
-(defn -main [& args]
-  (println (count part1) (count part2)))
+(defn -main [& args] (println part1 part2))
