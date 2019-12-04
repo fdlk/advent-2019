7fe7099757f83b2749323e64575d544adc49d87d
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-04 09:34:24 +0100

    feat(day04): part 2

--- src/advent_2019/day04.clj
+++ src/advent_2019/day04.clj
@@ -1,22 +1,29 @@
-(ns advent-2019.day04
-  (:require [clojure.string :refer [split join]])
-  (:require [clojure.set :refer [intersection]])
-  (:require [advent-2019.core :refer [lines parse-int manhattan]]))
+(ns advent-2019.day04)
 
-(def input (range 402328 (inc 864247)))
+(def input (map str (range 402328 (inc 864247))))
 
 (defn is-same [[d1 d2]] (= d1 d2))
+(def is-not-same (complement is-same))
 (defn is-ascending [[d1 d2]] (<= (int d1) (int d2)))
+(defn is-same-triple [[d1 d2 d3]]
+  (and (is-same [d1 d2]) (is-not-same [d2 d3])))
+(defn is-same-quad [[d1 d2 d3 d4]]
+  (and (is-same [d2 d3]) (is-not-same [d1 d2]) (is-not-same [d3 d4])))
 
-(defn is-valid-part1
-  [password]
-  (let [pairs (partition 2 1 (str password))]
-    (and 
-      (some? (some is-same pairs))
-      (every? is-ascending pairs))))
+(defn is-valid-part1 [password]
+  (let [pairs (partition 2 1 password)]
+    (and
+     (some? (some is-same pairs))
+     (every? is-ascending pairs))))
 
-(def part1 (count (filter is-valid-part1 input)))
+(defn is-also-valid-part2 [password]
+  (let [quads (partition 4 1 password)]
+    (or (is-same-triple (take 3 password))
+        (is-same-triple (take 3 (reverse password)))
+        (some? (some is-same-quad quads)))))
 
-(defn -main
-  [& args]
-  (println part1))
+(def part1 (filter is-valid-part1 input))
+(def part2 (filter is-also-valid-part2 part1))
+
+(defn -main [& args]
+  (println (count part1) (count part2)))
