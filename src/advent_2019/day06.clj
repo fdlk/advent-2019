fe6ed1992b4bfdae688c2c29e929ed2b2eb2b9e2
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-06 09:00:26 +0100

    feat(day06): part 2

--- src/advent_2019/day06.clj
+++ src/advent_2019/day06.clj
@@ -10,12 +10,21 @@
 
 (defn orbits-to-center
   [object-id]
-  (->>
-   (iterate input object-id)
-   (take-while #(not= % "COM"))
-   (count)))
+  (->> (iterate input object-id)
+       (take-while #(not= % "COM"))))
 
-(def part1 (sum (map orbits-to-center (keys input))))
+(def part1
+  (->> (keys input)
+       (map orbits-to-center)
+       (map count)
+       (sum)))
+
+(def part2
+  (->> (concat (orbits-to-center "YOU") (orbits-to-center "SAN"))
+       (frequencies)
+       (filter #(= (second %) 1))
+       (count)
+       (+ -2)))
 
 (defn -main [& args]
-  (println part1))
+  (println part1 part2))
