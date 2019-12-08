16c8c31362c193f6e2eca9bf4b262b832161a255
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-08 12:19:05 +0100

    feat(day08): part 2

--- src/advent_2019/day08.clj
+++ src/advent_2019/day08.clj
@@ -1,7 +1,8 @@
 (ns advent-2019.day08
-  (:require [advent-2019.core :refer [parse-int sum lines]]))
+  (:require [advent-2019.core :refer [lines]])
+  (:require [clojure.string :refer [join]]))
 
-(def input
+(def layers
   (->> "day08.txt"
        (lines)
        (first)
@@ -9,10 +10,26 @@
        (map #(- % (int \0)))
        (partition (* 25 6))))
 
-(def part1 (->> input
+(def part1 (->> layers
                 (map frequencies)
                 (sort-by #(get % 0))
                 (first)))
 
+(defn pixel [values]
+  (->> values
+       (filter #(not= 2 %))
+       (first)))
+
+(def part2
+  (join \newline
+        (->> layers
+             (apply interleave)
+             (partition (count input))
+             (map pixel)
+             (map #(if (= % 1) "#" " "))
+             (partition 25)
+             (map join))))
+
 (defn -main [& args]
-  (println (* (get part1 1) (get part1 2))))
+  (println (* (get part1 1) (get part1 2)))
+  (println part2))
