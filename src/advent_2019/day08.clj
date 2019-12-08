f774d9ef72e43c3bb76bcd5b382a7116ed6e831d
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-08 20:35:02 +0100

    refactor(day08): use chars not ints

--- src/advent_2019/day08.clj
+++ src/advent_2019/day08.clj
@@ -5,23 +5,22 @@
 (def layers (->> "day08.txt"
                  (lines)
                  (first)
-                 (map #(- (int %) (int \0)))
                  (partition (* 25 6))))
 
 (def part1 (->> layers
                 (map frequencies)
-                (sort-by #(get % 0))
+                (sort-by #(get % \0))
                 (first)))
 
 (def part2 (->> layers
                 (apply interleave)
                 (partition (count layers))
-                (map (fn [pixels] (first (filter #(not= % 2) pixels))))
-                (map #(if (= % 1) "#" " "))
+                (map (fn [pixels] (first (filter #(not= % \2) pixels))))
+                (map #(if (= % \1) "#" " "))
                 (partition 25)
                 (map join)
                 (join \newline)))
 
 (defn -main [& args]
-  (println (* (get part1 1) (get part1 2)))
+  (println (* (get part1 \1) (get part1 \2)))
   (println part2))
