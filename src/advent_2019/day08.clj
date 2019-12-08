4fba367bda67dce63c08771852678873fd287684
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-08 12:24:15 +0100

    refactor(day08): clean up

--- src/advent_2019/day08.clj
+++ src/advent_2019/day08.clj
@@ -2,33 +2,25 @@
   (:require [advent-2019.core :refer [lines]])
   (:require [clojure.string :refer [join]]))
 
-(def layers
-  (->> "day08.txt"
-       (lines)
-       (first)
-       (map int)
-       (map #(- % (int \0)))
-       (partition (* 25 6))))
+(def layers (->> "day08.txt"
+                 (lines)
+                 (first)
+                 (map #(- (int %) (int \0)))
+                 (partition (* 25 6))))
 
 (def part1 (->> layers
                 (map frequencies)
                 (sort-by #(get % 0))
                 (first)))
 
-(defn pixel [values]
-  (->> values
-       (filter #(not= 2 %))
-       (first)))
-
-(def part2
-  (join \newline
-        (->> layers
-             (apply interleave)
-             (partition (count input))
-             (map pixel)
-             (map #(if (= % 1) "#" " "))
-             (partition 25)
-             (map join))))
+(def part2 (->> layers
+                (apply interleave)
+                (partition (count layers))
+                (map (fn [pixels] (first (filter #(not= % 2) pixels))))
+                (map #(if (= % 1) "#" " "))
+                (partition 25)
+                (map join)
+                (join \newline)))
 
 (defn -main [& args]
   (println (* (get part1 1) (get part1 2)))
