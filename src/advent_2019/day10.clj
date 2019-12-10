63e954bd8abd9fefa2e080c5f2f8b5b5226cf5dc
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-10 23:37:46 +0100

    feat(day10): part 2

--- src/advent_2019/day10.clj
+++ src/advent_2019/day10.clj
@@ -10,7 +10,7 @@
                 (vec)))
 
 (def asteroids
-   (set (for [y (range (count input))
+  (set (for [y (range (count input))
              x (range (count (first input)))
              :when (= (get-in input [y x]) \#)]
          [x y])))
@@ -21,23 +21,18 @@
   (+ (* xa xb) (* ya yb)))
 
 (defn codirectional
-  "Determines if two vectors are codirectinal"
+  "Determines if two vectors are codirectional and aligned"
   [a b]
   (and
-    (pos-int? (inner-product a b))
-    (= (expt (inner-product a b) 2)
+   (pos-int? (inner-product a b))
+   (= (expt (inner-product a b) 2)
       (* (inner-product a a)
-          (inner-product b b)))))
-
-(defn vec-sub
-  "Subtracts two vectors"
-  [a b]
-  (map - a b))
+         (inner-product b b)))))
 
 (defn obstructs
   "Determines if b obstructs a's view of c"
   [a b c]
-  (let [ab (vec-sub b a) ac (vec-sub c a)]
+  (let [ab (map - b a) ac (map - c a)]
     (and
      (codirectional ab ac)
      (< (inner-product ab ab) (inner-product ac ac)))))
@@ -52,13 +47,20 @@
 (defn visible-asteroids
   "The asteroids visible from a"
   [a]
- (filter (fn [c] (can-see-from a c)) (disj asteroids a)))
+  (filter (partial can-see-from a) (disj asteroids a)))
 
 (defn number-of-visible-asteroids
   "The number of asteroids visible from a"
   [a]
   (count (visible-asteroids a)))
 
-(def part1 (reduce max (map number-of-visible-asteroids asteroids)))
+(def part1 (apply max-key first (map (fn [a] [(number-of-visible-asteroids a) a]) asteroids)))
 
-(defn -main [& args] (println part1))
+(def part2
+  (let [[_ [x0 y0]] part1]
+    (nth (sort-by
+          (fn [[x y]] (- (Math/atan2  (- x x0) (- y y0))))
+          (visible-asteroids [x0 y0]))
+         (dec 200))))
+
+(defn -main [& args] (println part1 part2))
