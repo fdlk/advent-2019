fb65e21a4da6bcd250b0548b1a8e640c802c5485
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-15 00:18:03 +0100

    feat(day14): part 2

--- src/advent_2019/day14.clj
+++ src/advent_2019/day14.clj
@@ -47,7 +47,7 @@
         product (:name tuple)
         reaction (reaction-map product)
         produced-per-execution (get-in reaction [:product :count])
-        number-of-times (int (ceil (/ desired produced-per-execution)))]
+        number-of-times (bigint (ceil (/ desired produced-per-execution)))]
     (:ingredients (multiply-reaction number-of-times reaction))))
 
 (defn combine-ingredients
@@ -73,14 +73,32 @@
         not-needed (filter (fn [candidate] (not-any? #(= (:name candidate) %) ingredients)) needed)]
     (filter #(not= "ORE" (:name %)) not-needed)))
 
-(def part1
-  (loop [needed (what-do-i-need {:count 1, :name "FUEL"})]
+(defn how-much-ore-for
+  [count]
+  (loop [needed (what-do-i-need {:count count, :name "FUEL"})]
     (let [ingredient-list (what-next needed)]
-      (if (empty? ingredient-list) needed
+      (if (empty? ingredient-list) (:count (first needed))
           (let [ingredient (first ingredient-list)
                 the-rest (filter (partial not= ingredient) needed)
                 needed-for-ingredient (what-do-i-need ingredient)
                 combined (combine-ingredients needed-for-ingredient the-rest)]
             (recur combined))))))
 
-(defn -main [& _] (println "day14" part1))
+(def part1 (how-much-ore-for 1))
+
+(defn bisect
+  [f a0 b0]
+  (loop [a a0 b b0]
+  (let [mid (quot (+ a b) 2)
+        comparison (f mid)]
+    (if (= mid a) a
+        (case comparison
+          0 mid
+          1 (recur a mid)
+          -1 (recur mid b))))))
+
+(def trillion 1000000000000N)
+
+(def part2 (bisect #(compare (how-much-ore-for %) trillion) (quot trillion part1) trillion))
+
+(defn -main [& _] (println "day14" part1 part2))
