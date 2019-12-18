32772e172214451d4ff0c39cf0040b3d1d7ab161
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-18 09:51:00 +0100

    feat(day18): add A*

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [org.clojure/clojure "1.10.0"]
                  [criterium "0.4.5"]
                  [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/data.priority-map "0.0.10"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot advent-2019.day18
   :target-path "target/%s"
--- src/advent_2019/day18.clj
+++ src/advent_2019/day18.clj
@@ -1,6 +1,6 @@
 (ns advent-2019.day18
-  (:require [advent-2019.core :refer [lines parse-int]])
-  (:require [clojure.string :refer [lower-case]]))
+  (:require [advent-2019.core :refer [lines]])
+  (:require [clojure.data.priority-map :refer [priority-map]]))
 
 (def maze (lines "day18.txt"))
 
@@ -18,5 +18,33 @@
                   key-found
                   (keys (Character/toLowerCase map-char)))]
     [x y (if key-found (conj keys map-char) keys)]))
+
+; source: http://clj-me.cgrand.net/2010/09/04/a-in-clojure/
+(defn A*
+  "Finds a path between start and goal inside the graph described by edges
+  (a map of edge to distance); estimate is an heuristic for the actual
+  distance. Accepts a named option: :monotonic (default to true).
+  Returns the path if found or nil."
+  [edges estimate start goal & {mono :monotonic :or {mono true}}]
+  (let [f (memoize #(estimate % goal)) ; unsure the memoization is worthy
+        neighbours (reduce (fn [m [a b]] (assoc m a (conj (m a #{}) b)))
+                           {} (keys edges))]
+    (loop [q (priority-map start (f start))
+           preds {}
+           shortest {start 0}
+           done #{}]
+      (when-let [[x hx] (peek q)]
+        (if (= goal x)
+          (reverse (take-while identity (iterate preds goal)))
+          (let [dx (- hx (f x))
+                bn (for [n (remove done (neighbours x))
+                         :let [hn (+ dx (edges [x n]) (f n))
+                               sn (shortest n Double/POSITIVE_INFINITY)]
+                         :when (< hn sn)]
+                     [n hn])]
+            (recur (into (pop q) bn)
+                   (into preds (for [[n] bn] [n x]))
+                   (into shortest bn)
+                   (if mono (conj done x) done))))))))
 
 (defn -main [& _] (println "day18"))
