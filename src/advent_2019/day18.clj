dd6a15f1603b993723d691f8a29758a616e79358
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-18 09:40:57 +0100

    feat(day18): neighbors function with keys

--- src/advent_2019/day18.clj
+++ src/advent_2019/day18.clj
@@ -1,7 +1,22 @@
 (ns advent-2019.day18
   (:require [advent-2019.core :refer [lines parse-int]])
-  (:require [clojure.string :refer [split trim]]))
+  (:require [clojure.string :refer [lower-case]]))
 
-(def input (lines "day18.txt"))
+(def maze (lines "day18.txt"))
 
-(defn -main [& _] (println "day18" input))
+(defn grid-neighbors
+  [p]
+  (map (partial map + p) [[0 -1] [0 1] [-1 0] [1 0]]))
+
+(defn neighbors
+  [[x0 y0 keys]]
+  (for [neighbor (grid-neighbors [x0 y0])
+        :let [[x y] neighbor
+              map-char (get-in maze [y x])
+              key-found (Character/isLowerCase map-char)]
+        :when (or (#{\. \@} map-char)
+                  key-found
+                  (keys (Character/toLowerCase map-char)))]
+    [x y (if key-found (conj keys map-char) keys)]))
+
+(defn -main [& _] (println "day18"))
