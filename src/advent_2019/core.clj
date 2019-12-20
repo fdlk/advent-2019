5e2fc3f435a37bac14f473b74b638d27e255a0c2
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-20 22:01:27 +0100

    feat(day20): part 1

--- src/advent_2019/core.clj
+++ src/advent_2019/core.clj
@@ -1,6 +1,7 @@
 (ns advent-2019.core
   (:require [clojure.java.io :refer [resource]])
   (:require [clojure.string :refer [split-lines]])
+  (:require [clojure.data.priority-map :refer [priority-map]])
   (:gen-class))
 
 (defn parse-int
@@ -17,6 +18,10 @@
   "Determines the manhattan distance between two points"
   [[x1 y1] [x2 y2]]
   (+ (Math/abs (- x1 x2)) (Math/abs (- y1 y2))))
+
+(defn grid-neighbors
+  [p]
+  (map (partial map + p) [[0 -1] [0 1] [-1 0] [1 0]]))
 
 (defn lines
   [name]
@@ -35,3 +40,30 @@
 (defn lcm
   [a b]
   (/ (* a b) (gcd a b)))
+
+; ; source: http://clj-me.cgrand.net/2010/09/04/a-in-clojure/
+; ; adapted for this problem
+(defn A*
+  "Finds a path between start and goal inside the graph described by edges
+  (a map of edge to distance); estimate is an heuristic for the actual
+  distance. Accepts a named option: :monotonic (default to true).
+  Returns the path if found or nil."
+  [neighbor-fn estimate start are-we-done & {mono :monotonic :or {mono true}}]
+  (let [f (memoize #(estimate %))] ; unsure the memoization is worthy
+    (loop [q (priority-map start (f start))
+           preds {}
+           shortest {start 0}
+           done #{}]
+      (when-let [[x hx] (peek q)]
+        (if (are-we-done x)
+          (reverse (take-while identity (iterate preds x)))
+          (let [dx (- hx (f x))
+                bn (for [n (remove done (neighbor-fn x))
+                         :let [hn (+ dx 1 (f n))
+                               sn (shortest n Double/POSITIVE_INFINITY)]
+                         :when (< hn sn)]
+                     [n hn])]
+            (recur (into (pop q) bn)
+                   (into preds (for [[n] bn] [n x]))
+                   (into shortest bn)
+                   (if mono (conj done x) done))))))))
--- src/advent_2019/day20.clj
+++ src/advent_2019/day20.clj
@@ -1,4 +1,57 @@
 (ns advent-2019.day20
-  (:require [clojure.math.numeric-tower :refer [floor ceil]]))
+  (:require [advent-2019.core :refer [lines manhattan grid-neighbors A*]])
+  (:require [clojure.string :refer [join]]))
 
-(defn -main [& _] (println "day20"))
+(def maze (lines "day20.txt"))
+
+(def maze-map
+  (into {}
+        (for [y (range (count maze))
+              x (range (count (first maze)))
+              :let [character (get-in maze [y x])]
+              :when (not= character \space )]
+          [[x y] character])))
+
+(def portals
+  (for [[[x y] character] (seq maze-map)
+              :when (not (#{\# \.} character))
+              entrance (grid-neighbors [x y])
+              :when (= (maze-map entrance) \.)
+              :let [direction (map - [x y] entrance)
+                    other-char-loc (map + [x y] direction)
+                    other-char (maze-map other-char-loc)]]
+          [(join (sort [character other-char])) entrance]))
+
+(def portals-grouped (group-by first portals))
+
+(defn hop-through
+  "hops through a portal"
+  [location]
+  (let [portal-name (first (first (filter #(= location (second %)) portals)))
+        candidates (portals-grouped portal-name)
+        exit (first (filter #(not= location (second %)) candidates))]
+    (second exit)))
+
+(defn neighbors
+  [location]
+  (for [candidate (grid-neighbors location)
+        :let [glyph (maze-map candidate)]
+        :when (not= glyph \#)
+        :let [result (if (= \. glyph)
+                       candidate
+                       (hop-through location))]
+        :when (not= result nil)]
+    result))
+
+; (defn heuristic
+;   [[x y keys-found]]
+;   (->> keys-in-maze
+;        (filter (fn [[_ _ character]] (not (keys-found character))))
+;        (map (fn [[xkey ykey _]] (manhattan [x y] [xkey ykey])))
+;        (reduce min 0)))
+
+(def start (get-in portals-grouped ["AA" 0 1]))
+(def finish (get-in portals-grouped ["ZZ" 0 1]))
+(def part1 (A* neighbors (constantly 0) start #{finish}))
+
+(defn -main [& _] (println "day20" (dec (count part1))))
