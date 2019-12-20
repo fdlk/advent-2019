ba3fa751dd283e8375a0cbacd044cc7db366aa0f
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-20 19:52:20 +0100

    chore(day20): set up day 20

--- project.clj
+++ project.clj
@@ -3,13 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-                 [org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [criterium "0.4.5"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.priority-map "0.0.10"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :main ^:skip-aot advent-2019.day18
+  :main ^:skip-aot advent-2019.day20
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.6.6"]]
   :jvm-opts ["-Xmx16g"]
---
+++ src/advent_2019/day20.clj
@@ -0,0 +1,4 @@
+(ns advent-2019.day20
+  (:require [clojure.math.numeric-tower :refer [floor ceil]]))
+
+(defn -main [& _] (println "day20"))
