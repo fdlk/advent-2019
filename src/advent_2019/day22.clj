1c88947052de1a1b075f06cc9b8fe809a7d9a990
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-22 14:09:07 +0100

    feat(day22): part 1

--- src/advent_2019/day22.clj
+++ src/advent_2019/day22.clj
@@ -1,7 +1,7 @@
 (ns advent-2019.day22
-  (:require [advent-2019.intcode :refer [run]])
-  (:require [clojure.string :refer [starts-with?]])
   (:require [advent-2019.core :refer [lines parse-int]]))
+
+(def deck-size 10007)
 
 (defn parse
   [line]
@@ -15,8 +15,48 @@
           [:deal-increment (parse-int (second deal-match))]
           nil)))))
 
+(defn deal-stack 
+  [deck] 
+  (reverse deck))
+
+(defn cut 
+  [amount deck]
+  (if (>= amount 0)
+    (concat (drop amount deck) (take amount deck))
+    (cut (+ deck-size amount) deck)))
+
+(defn deal-with-increment
+  [increment deck]
+  (let [quotient (quot deck-size increment)
+        remainder (rem deck-size increment)
+        leftover (- increment remainder)]
+    (loop [to-deal deck
+           stacks (vec (repeat increment nil))
+           index 0]
+      (if (empty? to-deal)
+        (take deck-size (apply interleave stacks))
+        (let [deal-one-extra (< index remainder)
+              amount-to-deal (if deal-one-extra (inc quotient) quotient)
+              stack (take amount-to-deal to-deal)]
+          (recur (drop amount-to-deal to-deal) 
+                 (assoc stacks index (if (not deal-one-extra) (concat stack [nil]) stack))
+                 (rem (+ leftover index) increment)))))))
+
+(defn shuffle-step
+  [deck technique]
+  (case (first technique)
+    :deal-stack (deal-stack deck)
+    :cut (cut (second technique) deck)
+    :deal-increment (deal-with-increment (second technique) deck)))
+
+(defn shuffle-deck
+  [deck techniques]
+  (reduce shuffle-step deck techniques))
+
 (def input (->> "day22.txt"
                 (lines)
                 (map parse)))
 
-(defn -main [& _] (println "Day 22" input))
+(def part1 (.indexOf (shuffle-deck (range deck-size) input) 2019))
+
+(defn -main [& _] (println "Day 22" part1))
