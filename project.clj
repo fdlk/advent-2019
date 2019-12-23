84a3bff144fbdf666f4445251c655dfe92c361ac
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-23 22:34:36 +0100

    feat(day23): buggy part 1

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.priority-map "0.0.10"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :main ^:skip-aot advent-2019.day22
+  :main ^:skip-aot advent-2019.day23
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.6.6"]]
   :jvm-opts ["-Xmx16g"]
